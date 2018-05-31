d1e828ce506970e7564ae13eb51af4ce73a15704
Damir Pristav <damirpristav@gmail.com>
2018-05-31 18:50:32 +0200

    prepared for deploy

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 // define express app
 const app = express();
@@ -43,6 +44,15 @@ app.get('/', (req, res) => {
 // use routes
 app.use('/api/users', users);
 app.use('/api/quotes', quotes);
+
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production'){
+    // Set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 // define port
 const port = process.env.PORT || 5000;
